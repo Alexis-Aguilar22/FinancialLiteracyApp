4d31fc993e3663800184d0ad72d8992e6a3543f6
Alexis-Aguilar22 <alexis.aguilar1552@gmail.com>
2024-07-02 10:31:17 -0500

    Add files via upload

---
+++ FinancialLiteracyApp/FinancialLiteracyApp/TabFour.swift
@@ -0,0 +1,20 @@
+//
+//  TabFour.swift
+//  FinancialLiteracyApp
+//
+//  Created by sermacbook_20 on 7/2/24.
+//
+
+import SwiftUI
+
+struct TabFour: View {
+    var body: some View{
+    Text("Learn")
+            .fontWeight(.heavy)
+            .foregroundColor(Color.green)
+    }
+}
+
+#Preview {
+    TabFour()
+}
---
+++ FinancialLiteracyApp/FinancialLiteracyApp/TabOne.swift
@@ -0,0 +1,21 @@
+//
+//  TabOne.swift
+//  FinancialLiteracyApp
+//
+//  Created by sermacbook_20 on 7/2/24.
+//
+
+import SwiftUI
+
+struct TabOne: View {
+    var body: some View{
+    Text("Screen 1")
+            .fontWeight(.heavy)
+            .foregroundColor(Color.red)
+    
+    }
+}
+
+#Preview {
+    TabOne()
+}
